25b6a916ff6f6e05487d5b653e1017a422caa721
Lucas Iankowski Corrêa da Silva <lucasicsilva@hotmail.com>
2020-09-09 19:43:32 -0300

    Add files via upload

--- Filas.py
+++ Filas.py
@@ -2,11 +2,24 @@
 
 class Filas:
 
-    def __init__(self, lista):
+    def __init__(self, ocupantes, lista):
+        self.ocupantes = ocupantes
         self.lista = lista
+        f = []
+        for _ in range(lista):
+            f.append(0)
 
-    def chegada():
-        if(len(lista) < size):
-            lista.append(1)
+    def getOcupantes(self):
+        return self.ocupantes
 
-    def saida():
+    def getLista(self):
+        return self.lista
+
+    def atualizaTempo(self, index, elem):
+        self.lista[index] += elem
+
+    def addOcupante(self):
+        self.ocupantes = self.ocupantes + 1
+
+    def subOcupante(self):
+        self.ocupantes = self.ocupantes - 1
--- main.py
+++ main.py
@@ -6,9 +6,12 @@ tempo_global = 0
 
 
 def main():
-    fila1 = Filas(200)
-    fila2 = Filas(100)
-    fila3 = Filas(1000)
+    fila1 = Filas(0, 200)
+    fila2 = Filas(0, 100)
+    fila3 = Filas(0, 1000)
+
+    tempoAnterior = 0
+
     agenda = []
 
     resultado = calcula(120, 7, 1000, 5)
@@ -18,22 +21,40 @@ def main():
         menorTempo = Math.inf()
         indiceRetirada = 0
         for index in agenda:
-            if menorTempo > index.getTempo:
-                menorTempo = index.getTempo
+            if menorTempo > index.getTempo():
+                menorTempo = index.getTempo()
                 indiceRetirada = index
+
         if agenda[indiceRetirada].getEvento() == "chegada":
+            fila1.atualizaTempo(fila1.getOcupantes(), index.getTempo() - tempoAnterior))
+            tempoAnterior=index.getTempo()
+            if fila1.getOcupantes < 200:
+                fila1.addOcupante()
+                if fila1.getOcupantes() <= 1:
+                    agenda.append(Agenda.agendamentoSaida(
+                        index.getTempo(), resultado[0]))
+                    resultado.remove(0)
             agenda.append(Agenda.agendamentoChegada(
-                index.getTempo, resultado[0]))
+                index.getTempo(), resultado[0]))
             resultado.remove(0)
+
+        if agenda[indiceRetirada].getEvento() == "saida":
+            fila1.atualizaTempo(fila1.getOcupantes(), index.getTempo() - tempoAnterior))
+            tempoAnterior=index.getTempo()
+            fila1.subOcupante()
+            if fila1.getOcupantes() >= 1
+                agenda.append(Agenda.agendamentoSaida(
+                    index.getTempo(), resultado[0]))
+                resultado.remove(0)
         resultado.remove(indiceRetirada)
 
 # Sorteia os números pseudo-aleatórios
 
 
 def calcula(x0, a, m, c):
-    resultados = []
+    resultados=[]
     for index in range(m):
-        resultado = (a * x0 + c) % m
-        x0 = resultado
+        resultado=(a * x0 + c) % m
+        x0=resultado
         resultados.append(resultado)
     return resultados
